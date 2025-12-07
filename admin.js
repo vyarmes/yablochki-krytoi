5625923b7ea80661a4c66cd25c1834ee1a438871
vyarmes <grigorenkosasha67@gmail.com>
2025-12-07 10:52:10 +0200

    туц

--- admin.js
+++ admin.js
@@ -1,50 +1,28 @@
 let productForm = document.getElementById('add_product_form');
 
-productForm.addEventListener('submit', function(event){
+productForm.addEventListener('submit', function(event) {
     event.preventDefault();
+
     let data = JSON.stringify({
-        "name": event.target['name'].value,
-        "description": event.target['description'].value,
-        "price": event.target['price'].value,
-        "photo_url": event.target['photo_url'].value
+        name: event.target['name'].value,
+        description: event.target['description'].value,
+        price: event.target['price'].value,
+        photo_url: event.target['photo_url'].value
     });
 
-    // let xhr = new XMLHttpRequest();
-    // xhr.withCredentials = false;
+    let xhr = new XMLHttpRequest();
+    xhr.withCredentials = false;
 
-    // xhr.addEventListener("readystatechange", function(){
-    //     if (this.readyState === 4) {
-    //         console.log(this.responseText);
-    //     }
-    // });
+    xhr.addEventListener("readystatechange", function() {
+        if (this.readyState === 4) {
+            console.log(this.responseText);
+        }
+    });
 
-//     xhr.open("POST", "https://yablochki-2d82.restdb.io/rest/product");
-//     xhr.setRequestHeader("content-type", "application/json");
-//     xhr.setRequestHeader("x-apikey", "69343d861c64b97709dde62d");
-//     xhr.setRequestHeader("cache-control", "no-cache");
+    xhr.open("POST", "https://yablochki-2d82.restdb.io/rest/product");
+    xhr.setRequestHeader("content-type", "application/json");
+    xhr.setRequestHeader("x-apikey", "69343d881c64b95446dde62e");
+    xhr.setRequestHeader("cache-control", "no-cache");
 
     xhr.send(data);
-})
-
-// var data = JSON.stringify({
-//   "field1": "name",
-//   "field2": "description",
-//   "field3": "photo_url",
-//   "field4": "price"
-// });
-
-var xhr = new XMLHttpRequest();
-xhr.withCredentials = false;
-
-xhr.addEventListener("readystatechange", function () {
-  if (this.readyState === 4) {
-    console.log(this.responseText);
-  }
 });
-
-xhr.open("POST", "https://yablochki-2d82.restdb.io/rest/product");
-xhr.setRequestHeader("content-type", "application/json");
-xhr.setRequestHeader("x-apikey", "69343d881c64b95446dde62e");
-xhr.setRequestHeader("cache-control", "no-cache");
-
-// xhr.send(data);
